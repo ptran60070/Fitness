3015cf8c2baf0169c373748f7ff2d07040d51c17
Phuong Tran <51715075+ptran60070@users.noreply.github.com>
2021-12-10 10:35:44 -0800

    Add all files

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $(window).on('scroll load',function(){
+    
+       if($(window).scrollTop() > 60){
+            $('header').addClass('header-active');
+        }else{
+            $('header').removeClass('header-active');
+        }
+    
+        $('section').each(function(){
+    
+            let top = $(window).scrollTop();
+            let height = $(this).height();
+            let offset = $(this).offset().top - 200;
+            let id = $(this).attr('id');
+    
+            if(top >= offset && top < offset + height){
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+    
+        });
+    
+    });
+    })
+    
